bc89ef90cf3701844ed67e58b1d5b26431c5f9ed
Tapiwa Tsopotsa <tapiwatsopotsa1@gmail.com>
2023-03-21 11:42:23 +0200

    routing set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     ContactComponent,
   ],
   imports: [BrowserModule, NgbModule, AppRoutingModule],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -2,11 +2,33 @@ import { Component, Input } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 
+import { DishService } from '../services/dish.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
 })
 export class DishdetailComponent {
-  @Input() dish!: Dish;
+  dish!: Dish;
+
+  constructor(
+    private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    // this.dish = this.dishservice.getDish(id);
+    this.dish = this.dishservice.getDish(id.toString());
+
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
