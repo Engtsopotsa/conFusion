6afe73e0cfd1d757945301fd25de2eb6e42ef6fe
Tapiwa Tsopotsa <tapiwatsopotsa1@gmail.com>
2023-04-01 00:43:28 +0200

    login work

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {
+  NgbModal,
+  ModalDismissReasons,
+  NgbDatepickerModule,
+} from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private modalService: NgbModal) {}
 
+  openLoginModal() {
+    const modalRef = this.modalService.open(LoginComponent);
+    // Do any additional setup of the modal, such as setting options or passing data to the component
+  }
 }
